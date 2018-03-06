0b7eb8bdfefc8e574a06a15c36bfa9345e98b740
m4kvn <fun.masahiro@gmail.com>
2018-03-07 00:32:50 +0900

    Update

--- symboliclink.sh
+++ symboliclink.sh
@@ -1,2 +1,4 @@
 dotfileDir=$(cd $(dirname $0) && pwd)
-cd $HOME && ls -A ${dotfileDir}/dots | xargs -I{} ln -s ${dotfileDir}/dots/{}
+cd $HOME && \
+find ${dotfileDir}/dots -type f | \
+xargs -I{} ln -s {} 2>/dev/null
