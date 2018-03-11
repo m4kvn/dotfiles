391040ee88fe04125c48691565caf46dffd32477
m4kvn <18133161+m4kvn@users.noreply.github.com>
2018-03-12 02:20:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 dir=$HOME/.dotfiles
 [ -d "$dir" ] && echo "$dir is already exists" && exit 1
-git clone https://github.com/m4kvn/scripts.git $dir
+git clone https://github.com/m4kvn/dotfiles.git $dir
 sh $dir/symboliclink.sh
